35d10311bc8f675d4d5e915e89239b4550601b90
camisev25 <camilasevalho@gmail.com>
2025-12-16 10:34:07 +0000

    Added comments to explain the new code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// The next code line shows a list of recipes suitable for babies at weaning stage
 let recipeList = [{name:"Festive biscuits", suitabilityAgeInMonths: 9, types: ['breakfast', 'snack']}, 
 {name: "Banana ice lollies", suitabilityAgeInMonths: 9, types: ['dessert', 'snack']},
 {name: "Avocarbonara", suitabilityAgeInMonths: 9, types: ['main', 'sides']}]
