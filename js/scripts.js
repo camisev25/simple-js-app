658e8076a7c4aa3facdedc82032ec4487dad3df9
camisev25 <camilasevalho@gmail.com>
2026-01-19 14:06:59 +0000

    Updated the code to include forEach() loop in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,15 @@ let recipeList = [{name:"Festive biscuits", suitabilityAgeInMonths: 9, types: ['
 {name: "Avocarbonara", suitabilityAgeInMonths: 9, types: ['main', 'sides']}]
 
 // The next code line creates a list of recipes suitable for babies under 1yo and it points out which ones can be served as a snack
-for (let i=0; i<recipeList.length; i++) {
-    let message = recipeList[i].name + " is suitable for babies under 1yo";
+recipeList.forEach (function(recipe) {
+    let message = recipe.name + " is suitable for babies under 1yo";
 
-if (recipeList[i].types.includes('snack')) {
+if (recipe.types.includes('snack')) {
         message += " - you can serve this as a snack";
     } else {
         message += " - this is too heavy for a snack!"
     }
     
 document.write(message + "<br>");
-}
-    
+})
+
